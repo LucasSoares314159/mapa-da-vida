2cd93fcdb00f953f07384dcccb31ec684e5409dd
Lucas <luccascarvalho7@gmail.com>
2026-05-01 16:59:33 -0300

    feature de rotina alterações no front

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    optimizePackageImports: ['lucide-react', '@xyflow/react'],
+    optimizePackageImports: ['lucide-react', '@xyflow/react', '@xyflow/system', '@radix-ui/react-dialog', '@radix-ui/react-select', '@radix-ui/react-label', '@radix-ui/react-slot'],
   },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
